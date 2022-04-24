48aa70ca7eeda7c4d6aca03b9aecee1cf52d4e8b
WilsonKinyua <wilsonkinyuam@gmail.com>
2022-04-24 22:59:03 +0300

    footer section created

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  currentYear = new Date().getFullYear();
+
   constructor() { }
 
   ngOnInit(): void {
