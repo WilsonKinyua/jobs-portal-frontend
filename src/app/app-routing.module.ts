c09055d8aba2a4ca6cd9c9ce38a14f5efb027361
WilsonKinyua <wilsonkinyuam@gmail.com>
2022-04-24 21:57:41 +0300

    routing models set

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,41 @@
+import { JobsListComponent } from './components/jobs-list/jobs-list.component';
+import { JobDetailsComponent } from './components/job-details/job-details.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateJobComponent } from './components/create-job/create-job.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomepageComponent,
+  },
+  {
+    path: 'job-details',
+    component: JobDetailsComponent,
+  },
+  {
+    path: 'dashboard/create-a-job',
+    component: CreateJobComponent,
+  },
+  {
+    path: 'dashboard/jobs-list',
+    component: JobsListComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
